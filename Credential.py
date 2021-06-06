cfbc3afd43b4997d756dc8aa0c3513616d5d8ae8
yzmar4real <70125095+yzmar4real@users.noreply.github.com>
2021-06-06 10:56:26 +0400

    Credential script that contains APIC details

---
+++ Credential.py
@@ -0,0 +1,10 @@
+
+#Define username and password for accessing APIC controller
+
+apic_username = '<enter apic username here>'
+apic_password = '<enter apic password here>'
+
+#Define the IP addresses for the APIC cluster. If using a single instance, one of the ip's is enough 
+apic_dc_ip1 = '<apic cluster ip1>'
+apic_dc_ip2 = '<apic cluster ip2>'
+apic_dc_ip3 = '<apic cluster ip3>'
